94675a8429013c9361c4f50f4c091f8683c5d1f7
Alisson Silva <alissonjosef@gmail.com>
2022-06-01 21:30:12 -0300

    post view

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,84 +1,108 @@
-import React,{ useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import { View, Text, StyleSheet, Button, SafeAreaView,TouchableOpacity , FlatList} from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
-import { Feather } from '@expo/vector-icons'
+import { Feather } from "@expo/vector-icons";
 import api from "../../service/api";
 
-import CategoryItem  from '../../components/CategoryItem'
+import CategoryItem from "../../components/CategoryItem";
 import { getFavorite, setFavorite } from "../../service/favorite";
 import FavoritePost from "../../components/FavoritePost";
+import PostItem from "../../components/PostItem";
 
 export default function Home() {
   const navigation = useNavigation();
-  const [categories, setCategories] = useState([])
-  const [favCategory, setFavCategory] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [favCategory, setFavCategory] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function loadData(){
-      const category = await api.get("/api/categories?populate=icon")
-      setCategories(category.data.data)
+    async function loadData() {
+      await getListPosts();
+
+      const category = await api.get("/api/categories?populate=icon");
+      setCategories(category.data.data);
     }
 
-    loadData()
+    loadData();
   }, []);
+
+  async function getListPosts() {
+    const response = await api.get(
+      "api/posts?populate=cover&sort=createdAt:desc"
+    );
+    setPosts(response.data.data);
+  }
 
   useEffect(() => {
-    async function favorite(){
-      const response = await getFavorite()
-      setFavCategory(response)
+    async function favorite() {
+      const response = await getFavorite();
+      setFavCategory(response);
     }
   }, []);
 
-  async function handleFavorite(id){
-    const response = await setFavorite(id)
-    setFavCategory(response) 
+  async function handleFavorite(id) {
+    const response = await setFavorite(id);
+    setFavCategory(response);
   }
 
   return (
     <SafeAreaView style={style.container}>
+      <View style={style.header}>
+        <Text style={style.name}>DevBlog</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Search")}>
+          <Feather name="search" size={24} color="#FFF" />
+        </TouchableOpacity>
+      </View>
 
-        <View style={style.header}>
-            <Text style={style.name}>DevBlog</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('Search')}>
-                <Feather name='search' size={24} color='#FFF'/>
-            </TouchableOpacity>
-        </View>
-
-        <FlatList 
+      <FlatList
         showsHorizontalScrollIndicator={false}
         horizontal={true}
-        contentContainerStyle={{ paddingRight: 12}}
+        contentContainerStyle={{ paddingRight: 12 }}
         style={style.categories}
         data={categories}
         keyExtractor={(item) => String(item.id)}
-        renderItem={ ({item}) => (
-          <CategoryItem
-          data={item}
-          favorite={() => handleFavorite(item.id)}
-          />
+        renderItem={({ item }) => (
+          <CategoryItem data={item} favorite={() => handleFavorite(item.id)} />
         )}
-        />
+      />
 
-        <View style={style.main}>
-          {favCategory.length !== 0 && (
-            <FlatList 
-            style={{marginTop:50, maxHeight: 100, paddingStart: 18 ,}}
-            contentContainerStyle={{ paddingEnd: 18,}}
-            data={ favCategory}
+      <View style={style.main}>
+        {favCategory.length !== 0 && (
+          <FlatList
+            style={{ marginTop: 50, maxHeight: 100, paddingStart: 18 }}
+            contentContainerStyle={{ paddingEnd: 18 }}
+            data={favCategory}
             horizontal={true}
             showsHorizontalScrollIndicator={false}
-            keyExtractor={ (item) => String(item.id)}
-            renderItem={({item}) => <FavoritePost data={item}/>}
-            />
-          )}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({ item }) => <FavoritePost data={item} />}
+          />
+        )}
 
-          <Text style={[style.title,
-          {marginTop: favCategory.length > 0 ? 14 : 46}
-          ]}>Conteudo em alta</Text>
-        </View>
-      
+        <Text
+          style={[style.title, { marginTop: favCategory.length > 0 ? 14 : 46 }]}
+        >
+          Conteudo em alta
+        </Text>
+
+        <FlatList
+          style={{ flex: 1, paddingHorizontal: 18 }}
+          showsHorizontalScrollIndicator={false}
+          data={posts}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <PostItem data={item}/>}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -86,39 +110,38 @@ export default function Home() {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#232630'
+    backgroundColor: "#232630",
   },
-  header:{
-      flexDirection: 'row',
-      alignItems:   'center',
-      justifyContent: 'space-between',
-      marginHorizontal:18,
-      marginTop: 18,
-      marginBottom: 24
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 18,
+    marginTop: 18,
+    marginBottom: 24,
   },
   name: {
-      fontSize:28,
-      color: '#fff',
-      fontWeight: 'bold'
+    fontSize: 28,
+    color: "#fff",
+    fontWeight: "bold",
   },
-  categories:{
+  categories: {
     maxHeight: 115,
-    backgroundColor: '#EFEFEF',
-    marginHorizontal:18,
+    backgroundColor: "#EFEFEF",
+    marginHorizontal: 18,
     borderRadius: 8,
-    zIndex:9,
+    zIndex: 9,
   },
   main: {
     backgroundColor: "#FFF",
     flex: 1,
-    marginTop: -30
+    marginTop: -30,
   },
-  title:{
+  title: {
     fontSize: 21,
     paddingHorizontal: 18,
     marginBottom: 14,
-    fontWeight:'bold',
+    fontWeight: "bold",
     color: "#162133",
-    
-  }
+  },
 });
